3967cc3bf296744bbc732d850a113096ed06bc2c
Awienberg <a.wienberg@hotmail.com>
2019-10-30 12:45:39 +0100

    blank

--- main.js
+++ main.js
@@ -3,27 +3,29 @@
 //Get element by id
 var $ = function (foo) {
     return document.getElementById(foo);
-}
+};
 
-var createNewCanvas = $("create");
-var enterCanvasHeight = $("height");
-var enterCanvasWidth = $("width");
+//var createNewCanvas = $("create");
+
+//var enterCanvasWidth = $("width");
+
+//var enterCanvasHeight = $("heigh");  
 
 //Byg rum
-function createRoom() {
-    var enterCanvasHeightt = enterCanvasHeight.value;
-    var enterCanvasWidthh = enterCanvasWidth.value;
-    document.body.innerHTML = enterCanvasHeightt+":"+enterCanvasWidthh;
+    //function createRoom() {
     
-    var newCanvas = $("myCanvas1");
-    document.createElement("canvas");
-    newCanvas.classList.add("canvasDesign");
+    //var enterCanvasWidthh = enterCanvasWidth.value;
+    
+    //var enterCanvasHeightt = enterCanvasHeight.value;
+        
+    //var newCanvas = $("myCanvas1");
+    //document.createElement("canvas");
+    //newCanvas.classList.add("canvasDesign");
 
-    newCanvas.style.height = enterCanvasHeightt + "px";
-    newCanvas.style.width = enterCanvasWidthh + "px";
+    //newCanvas.style.width = enterCanvasWidthh + "px";
+    //newCanvas.style.height = enterCanvasHeightt + "px";
+        
+    //document.body.appendChild(newCanvas);
+//}
 
-
-    document.body.appendChild(newCanvas);
-}
-
-createNewCanvas.addEventListener('click', createRoom);
+//createNewCanvas.addEventListener('click', createRoom);
